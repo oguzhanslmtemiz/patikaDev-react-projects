fbe67de3fed2f002f046c0f734e1c0ed85ca5215
Oguzhan Selim Temiz <oguzhanselimtemiz@gmail.com>
2021-12-16 19:23:38 +0300

    Add daily weather

---
+++ weather-app/src/components/TodayWeather.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { WiHumidity, WiStrongWind, WiBarometer } from "react-icons/wi";
+import styles from "./TodayWeather.module.scss";
+
+export default function TodayWeather({
+  forecast,
+  imageLoaded,
+  setImageLoaded,
+  units,
+}) {
+  console.log("TODAY", forecast);
+  return (
+    <div className={styles.today}>
+      <div className={styles.city}>
+        {forecast.city + ", " + forecast.country}{" "}
+        <img
+          alt="flag"
+          src={`https://openweathermap.org/images/flags/${forecast.country.toLowerCase()}.png`}
+          className={styles.flag}
+        />
+      </div>
+      <div
+        className={styles.icon}
+        style={{ display: imageLoaded ? "block" : "none" }}
+      >
+        <img
+          src={`https://openweathermap.org/img/wn/${forecast.data.current.weather[0].icon}@4x.png`}
+          alt="forecast-icon"
+          onLoad={() => setImageLoaded(true)}
+        />
+      </div>
+      <div className={styles.desc}>
+        {forecast.data.current.weather[0].description}
+      </div>
+      <div className={styles.temp}>
+        {forecast.data.current.temp}°{units === "metric" ? "C" : "F"}
+      </div>
+      <div className={styles.other}>
+        <span>
+          <WiHumidity /> {forecast.data.current.humidity}
+        </span>
+        <span>
+          <WiStrongWind /> {forecast.data.current.wind_speed}
+          {units === "metric" ? " m/s" : " mph"}
+        </span>
+        <span>
+          <WiBarometer /> {forecast.data.current.pressure} hPa
+        </span>
+      </div>
+    </div>
+  );
+}
